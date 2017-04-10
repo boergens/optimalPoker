b49325566c6320330d1a23391aa480fc8e4629b3
Kevin Boergens <github@boerg.e4ward.com>
2017-04-10 17:05:47 +0200

    new folder structure

--- optimalPokerMeta.m
+++ optimalPokerMeta.m
@@ -3,5 +3,5 @@ for idx = 101:1000
     zz(idx) = optimalPoker();
     z = max(sum(zz(idx).payout_matrix .* repmat(cell2mat(money(:,2))',32, 1), 2))/50000;
     zzz(idx)= z;
-    savejson('solution', zz(idx),['games/solution_' num2str(idx-1) '.json'])
+    savejson('solution', zz(idx),['game/games/solution_' num2str(idx-1) '.json'])
 end
